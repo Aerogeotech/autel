17dd85821a9666a9a4c7a677be80bd15ef4de121
Wylky Ramirez <41131665+kingtoniormz@users.noreply.github.com>
2020-06-03 09:31:22 -0600

    portafolio
    portafolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 /*!
-    * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
+    propiedad inetelectual 
     */
     (function($) {
   "use strict"; // Start of use strict
