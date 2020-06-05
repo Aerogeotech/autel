24cf7b95fa290891b2f790bdeca6fd723ffe6f99
Wylky Ramirez <41131665+kingtoniormz@users.noreply.github.com>
2020-06-05 09:29:52 -0600

    preloader

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 /*!
     propiedad inetelectual 
     */
+
+   $(window).on('load', function () {
+    $('#preloader-active').delay(450).fadeOut('slow');
+    $('body').delay(450).css({
+      'overflow': 'visible'
+    });
+  });
     (function($) {
   "use strict"; // Start of use strict
 
